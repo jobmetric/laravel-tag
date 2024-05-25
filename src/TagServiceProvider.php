d713f7521410dc0618cc3b41ebc8a238db341775
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-25 20:26:53 +0330

    add service provider

---
+++ src/TagServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JobMetric\Tag;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class TagServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-tag')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation();
+    }
+}
