c6c7333c48b7cbced24ee9f7c7f0f62be86c3ac8
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-15 22:50:08 +0330

    add getTypeTag helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,31 @@
 <?php
+
+use Illuminate\Support\Collection;
+
+if (!function_exists('getTagType')) {
+    /**
+     * Get the tag type
+     *
+     * @param string|null $mode
+     *
+     * @return array
+     */
+    function getTagType(string $mode = null): array
+    {
+        $tagTypes = collect(app('tagType'));
+
+        if ($mode === 'key') {
+            return $tagTypes->keys()->toArray();
+        }
+
+        if ($mode === 'value') {
+            return $tagTypes->values()->map(function ($value) {
+                return trans($value);
+            })->toArray();
+        }
+
+        return $tagTypes->map(function ($value) {
+            return trans($value);
+        })->toArray();
+    }
+}
